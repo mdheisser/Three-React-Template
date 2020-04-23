6aa8387644d51167b60f7f68be3e762e6b3b507e
TopTalentDev <toptal715@gmail.com>
2020-04-23 22:52:43 +0200

    impl local model selection in loader + add models in resources

---
+++ src/resources/catalogs/Models.ts
@@ -0,0 +1,7 @@
+///<reference path="../../dts/misc-types-extend.d.ts" />
+import astronaut from '../assets/models/Astronaut.glf';
+import flamingo from '../assets/models/Flamingo.glb';
+import horse from '../assets/models/Horse.glb';
+
+// const astronaut = "/res";
+export { astronaut, flamingo, horse };
--- src/samples/ModelLoader.tsx
+++ src/samples/ModelLoader.tsx
@@ -1,33 +1,57 @@
 ///<reference path="../dts/misc-types-extend.d.ts" />
-import React, { useState, useRef, useEffect, Suspense } from "react";
-import { Box3, Vector3, Matrix4 } from "three";
-import { BoxSplitter } from "../components/Utils/BoxUtils";
-import { useFrame, useThree, Canvas } from "react-three-fiber";
-import { BoxEntityCtrlHlp } from "../components/Helpers/BoxEntityCtrlHlp";
-import { useSampleStates } from "../common/SampleStates";
-import { InfoOverlay } from "../components/UI/Overlay";
+import React, { Suspense, useEffect } from "react";
+import { Canvas, useThree } from "react-three-fiber";
+import { InfoOverlay, CaseSelector } from "../components/UI/Overlay";
 import { Controls, Wrapper, Helpers } from "./BasicDemo";
 
 import { useLoader } from 'react-three-fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import toto from '../resources/assets/img/sand.jpg'
+import { useSampleStates } from "../common/SampleStates";
+
+import * as Models from '../resources/catalogs/Models'
+import { focusOnObjects } from "../common/tools/CamUtils";
 
 function Asset({ url }: { url: string }) {
-    const gltf = useLoader(GLTFLoader, url)
+    const gltf = useLoader(GLTFLoader, url);
+    const ctrl = useSampleStates(state => state.controls);
+    const cam: any = useThree().camera;
+    const sce: any = useThree().scene;
+
+    useEffect(() => {
+        focusOnObjects([gltf.scene], 1.2, ctrl, cam);
+        console.log(sce)
+    })
+
     return <primitive object={gltf.scene} dispose={null} />
 }
 
+export default (/*{ sample }: any*/) => {
+    // (async () => {
+    //     try {
+    //         let response = await fetch(sample.assetUrl, {
+    //             mode: 'cors',
+    //        });
+    //         console.log(response)
+    //     } catch (e) {
+    //         console.log(e)
+    //     }
+    // })();
+    const sample = useSampleStates(state => state.sample);   // get sample from states instead of from props to subscribe updates
+    const caseNb: any = (sample.caseNb !== undefined && sample.caseNb !== null && sample.caseNb !== "") ? sample.caseNb : 1;
+    const models: any = Models;
+    const caseId: any = Object.keys(Models)[caseNb];
+    const model = models[caseId];
 
-export default ({ sample }: any) => {
     return (<>
         <InfoOverlay sample={sample} />
+        <CaseSelector sampleCases={Object.keys(Models)} caseId={caseNb} />
         <Canvas camera={{ position: [100, 50, 100] }}>
             <ambientLight intensity={2} />
             <Helpers size={128} />
             <Wrapper />
             <Controls />
             <Suspense fallback={null}>
-                <Asset url={sample.assetUrl} />
+                <Asset url={sample.assetUrl ? sample.assetUrl : model} />
             </Suspense>
         </Canvas>
     </>)
