c936ccbe8b1f82efea4efaa15b96712023f9a8fa
TopTalentDev <toptal715@gmail.com>
2020-04-29 01:28:56 +0200

    heigmapgen tool

--- src/samples/index.ts
+++ src/samples/index.ts
@@ -1,11 +1,30 @@
-import { lazy } from 'react'
-import { SAMPLE_TYPE } from '../common/constants';
+import { lazy } from "react";
+import { SAMPLE_TYPE } from "../common/constants";
 
-const BasicDemo = { tags: [SAMPLE_TYPE.DEMO], Component: lazy(() => import('./BasicDemo')) };
-const ModelLoader = { tags: [SAMPLE_TYPE.DEMO], Component: lazy(() => import('./ModelLoader')) };
-const BoxSplitDemo = { tags: [SAMPLE_TYPE.DEMO], Component: lazy(() => import('../samples/BoxSplitDemo')) };
-const TestBoxSplit = { tags: [SAMPLE_TYPE.TEST], Component: lazy(() => import('../samples/BoxSplit.test')) };
-const TestRaycastHlp = { tags: [SAMPLE_TYPE.TEST], Component: lazy(() => import('../samples/RaycastHlp.test')) };
+const BasicDemo = {
+  tags: [SAMPLE_TYPE.DEMO],
+  Component: lazy(() => import("./BasicDemo"))
+};
+const ModelLoader = {
+  tags: [SAMPLE_TYPE.DEMO],
+  Component: lazy(() => import("./ModelLoader"))
+};
+const BoxSplitDemo = {
+  tags: [SAMPLE_TYPE.DEMO],
+  Component: lazy(() => import("../samples/BoxSplitDemo"))
+};
+const TestBoxSplit = {
+  tags: [SAMPLE_TYPE.TEST],
+  Component: lazy(() => import("../samples/BoxSplit.test"))
+};
+const TestRaycastHlp = {
+  tags: [SAMPLE_TYPE.TEST],
+  Component: lazy(() => import("../samples/RaycastHlp.test"))
+};
+const HeightmapGenTool = {
+  tags: [SAMPLE_TYPE.TOOL],
+  Component: lazy(() => import("../samples/Tools/HeightmapGen"))
+};
 
 export {
   BasicDemo,
@@ -13,4 +32,5 @@ export {
   BoxSplitDemo,
   TestBoxSplit,
   TestRaycastHlp,
-}
+  HeightmapGenTool
+};
